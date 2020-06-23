a07a33c8109caff2744280f0d29e8a39c10b745d
MarionBWeinzierl <marion.weinzierl@durham.ac.uk>
2020-06-23 16:07:38 +0100

    #24

--- visualize_sample.py
+++ visualize_sample.py
@@ -32,7 +32,6 @@ for file in json_files:
     parser = OpenPoseJsonParser(os.path.join(path_to_json, file))
     body_keypoints_df = parser.get_multiple_keypoints([0, 1], OpenPosePartGroups.UPPER_BODY_PARTS, 0.7, previous_body_keypoints_df)
     body_keypoints_df.reset_index()
-    body_keypoints_df.reset_index()
     body_keypoints_dfs.append(body_keypoints_df)
     previous_body_keypoints_df = body_keypoints_df
 
