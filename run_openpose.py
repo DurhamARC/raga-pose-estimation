0076e7c7cba6441c0cb6d4a1fcdbccc54a4efe76
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-06-26 10:55:02 +0100

    Add options to cli script for body parts and confidence.
    Also ran black on this branch for the first time.

--- run_openpose.py
+++ run_openpose.py
@@ -5,6 +5,7 @@ import cv2
 
 from entimement_openpose.csv_writer import CSVWriter
 from entimement_openpose.openpose_json_parser import OpenPoseJsonParser
+from entimement_openpose.openpose_parts import OpenPosePartGroups, OpenPoseParts
 from entimement_openpose.visualizer import Visualizer
 
 
@@ -30,19 +31,44 @@ from entimement_openpose.visualizer import Visualizer
 @click.option('--height', default=0,
               help='Height of original video (mandatory for creating video if '
               ' not providing input-video)')
+@click.option('--confidence-threshold', default=0.0,
+              help='Confidence threshold. Items with a confidence lower than '
+              'the threshold will be replaced by values from a previous frame.')
+@click.option('--body-parts', default=None,
+              help='Body parts to include in output. Should be a '
+              'comma-separated list of strings as in the list at '
+              'https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/master/doc/output.md#keypoint-ordering-in-cpython'
+              ', e.g. "LEye,RElbow". Overrides --upper-body-parts and '
+              '--lower-body-parts.')
+@click.option('--upper-body-parts', 'bodypartsgroup', flag_value='upper',
+              default=True)
+@click.option('--lower-body-parts', 'bodypartsgroup', flag_value='lower')
 def openpose_cli(output_dir, openpose_dir, input_video,
                  input_json, create_model_video,
-                 create_overlay_video, width, height):
+                 create_overlay_video, width, height, confidence_threshold,
+                 body_parts, bodypartsgroup):
     """Runs openpose on the video, does post-processing, and outputs CSV
        files."""
+
+    if body_parts:
+        try:
+            body_parts_list = [OpenPoseParts(part) for part in body_parts.split(',')]
+        except ValueError as e:
+            click.echo(f'Invalid body-parts value {body_parts}: {e}')
+    elif bodypartsgroup == 'upper':
+        body_parts_list = OpenPosePartGroups.UPPER_BODY_PARTS
+    elif bodypartsgroup == 'lower':
+        body_parts_list = OpenPosePartGroups.LOWER_BODY_PARTS
+
     run_openpose(output_dir, openpose_dir, input_video,
                  input_json, create_model_video, create_overlay_video,
-                 width, height)
+                 width, height, confidence_threshold, body_parts_list)
 
 
 def run_openpose(output_dir, openpose_dir=None, input_video=None,
                  input_json=None, create_model_video=False,
-                 create_overlay_video=False, width=None, height=None):
+                 create_overlay_video=False, width=None, height=None,
+                 confidence_threshold=0, body_parts=None):
     """Runs openpose on the video, does post-processing, and outputs CSV files.
     Non-click version to work from jupyter notebooks."""
     if not os.path.isdir(output_dir):
@@ -94,20 +120,29 @@ def run_openpose(output_dir, openpose_dir=None, input_video=None,
     json_files = [pos_json for pos_json in os.listdir(path_to_json)
                   if pos_json.endswith('.json')]
 
+    if len(json_files) == 0:
+        print(f"No json files found in {path_to_json}. Exiting.")
+        exit(1)
+
     # Get array for dataframes
     body_keypoints_dfs = []
 
     # Loop through all json files in output directory
     # Each file is a frame in the video
     json_files.sort()
-    print(len(json_files))
+    previous_body_keypoints_df = None
+
     for file in json_files:
         parser = OpenPoseJsonParser(os.path.join(path_to_json, file))
-        body_keypoints_df = parser.get_multiple_keypoints([0, 1])
+        body_keypoints_df = parser.get_multiple_keypoints(
+            [0, 1],
+            body_parts,
+            confidence_threshold,
+            previous_body_keypoints_df
+        )
         body_keypoints_df.reset_index()
         body_keypoints_dfs.append(body_keypoints_df)
-
-    print(len(body_keypoints_dfs))
+        previous_body_keypoints_df = body_keypoints_df
 
     if create_model_video:
         print('Creating model video')
