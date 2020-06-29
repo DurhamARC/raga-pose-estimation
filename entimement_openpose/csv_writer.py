8ca1f8b14d4cd1b6acc83f05e0bf98e6e694504f
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-06-29 19:41:04 +0100

    Re-formatted new files using black

--- entimement_openpose/csv_writer.py
+++ entimement_openpose/csv_writer.py
@@ -3,12 +3,11 @@ import pandas as pd
 
 
 class CSVWriter:
-
     def writeCSV(dataframes, output_dir):
         n_people = n_people = len(dataframes[0].columns) // 3
 
         for i in range(n_people):
-            person_dfs = [df.iloc[:, i*3:i*3+3] for df in dataframes]
+            person_dfs = [df.iloc[:, i * 3 : i * 3 + 3] for df in dataframes]
             all_dfs = pd.concat(person_dfs, keys=range(len(person_dfs)))
 
-            all_dfs.to_csv(os.path.join(output_dir, f'person{i}.csv'))
+            all_dfs.to_csv(os.path.join(output_dir, f"person{i}.csv"))
--- run_openpose.py
+++ run_openpose.py
@@ -5,74 +5,136 @@ import cv2
 
 from entimement_openpose.csv_writer import CSVWriter
 from entimement_openpose.openpose_json_parser import OpenPoseJsonParser
-from entimement_openpose.openpose_parts import OpenPosePartGroups, OpenPoseParts
+from entimement_openpose.openpose_parts import (
+    OpenPosePartGroups,
+    OpenPoseParts,
+)
 from entimement_openpose.visualizer import Visualizer
 
 
 @click.command()
-@click.option('--input-video', default=None,
-              help='Path to the video file on which to run openpose')
-@click.option('--input-json', default=None,
-              help='Path to a directory of previously generated openpose '
-              'json files')
-@click.option('--output-dir', prompt='Output directory',
-              help='Path to the directory in which to output CSV files (and '
-              'videos if required).')
-@click.option('--openpose-dir',
-              help='Path to the directory in which openpose is installed.')
-@click.option('--create-model-video', is_flag=True, default=False,
-              help='Whether to create a video showing the poses on a blank '
-              'background')
-@click.option('--create-overlay-video', is_flag=True, default=False,
-              help='Whether to create a video showing the poses as an overlay')
-@click.option('--width', default=0,
-              help='Width of original video (mandatory for creating video if '
-              ' not providing input-video)')
-@click.option('--height', default=0,
-              help='Height of original video (mandatory for creating video if '
-              ' not providing input-video)')
-@click.option('--confidence-threshold', default=0.0,
-              help='Confidence threshold. Items with a confidence lower than '
-              'the threshold will be replaced by values from a previous frame.')
-@click.option('--body-parts', default=None,
-              help='Body parts to include in output. Should be a '
-              'comma-separated list of strings as in the list at '
-              'https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/master/doc/output.md#keypoint-ordering-in-cpython'
-              ', e.g. "LEye,RElbow". Overrides --upper-body-parts and '
-              '--lower-body-parts.')
-@click.option('--upper-body-parts', 'bodypartsgroup', flag_value='upper',
-              default=True)
-@click.option('--lower-body-parts', 'bodypartsgroup', flag_value='lower')
-def openpose_cli(output_dir, openpose_dir, input_video,
-                 input_json, create_model_video,
-                 create_overlay_video, width, height, confidence_threshold,
-                 body_parts, bodypartsgroup):
+@click.option(
+    "--input-video",
+    default=None,
+    help="Path to the video file on which to run openpose",
+)
+@click.option(
+    "--input-json",
+    default=None,
+    help="Path to a directory of previously generated openpose " "json files",
+)
+@click.option(
+    "--output-dir",
+    prompt="Output directory",
+    help="Path to the directory in which to output CSV files (and "
+    "videos if required).",
+)
+@click.option(
+    "--openpose-dir",
+    help="Path to the directory in which openpose is installed.",
+)
+@click.option(
+    "--create-model-video",
+    is_flag=True,
+    default=False,
+    help="Whether to create a video showing the poses on a blank "
+    "background",
+)
+@click.option(
+    "--create-overlay-video",
+    is_flag=True,
+    default=False,
+    help="Whether to create a video showing the poses as an overlay",
+)
+@click.option(
+    "--width",
+    default=0,
+    help="Width of original video (mandatory for creating video if "
+    " not providing input-video)",
+)
+@click.option(
+    "--height",
+    default=0,
+    help="Height of original video (mandatory for creating video if "
+    " not providing input-video)",
+)
+@click.option(
+    "--confidence-threshold",
+    default=0.0,
+    help="Confidence threshold. Items with a confidence lower than "
+    "the threshold will be replaced by values from a previous frame.",
+)
+@click.option(
+    "--body-parts",
+    default=None,
+    help="Body parts to include in output. Should be a "
+    "comma-separated list of strings as in the list at "
+    "https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/master/doc/output.md#keypoint-ordering-in-cpython"
+    ', e.g. "LEye,RElbow". Overrides --upper-body-parts and '
+    "--lower-body-parts.",
+)
+@click.option(
+    "--upper-body-parts", "bodypartsgroup", flag_value="upper", default=True
+)
+@click.option("--lower-body-parts", "bodypartsgroup", flag_value="lower")
+def openpose_cli(
+    output_dir,
+    openpose_dir,
+    input_video,
+    input_json,
+    create_model_video,
+    create_overlay_video,
+    width,
+    height,
+    confidence_threshold,
+    body_parts,
+    bodypartsgroup,
+):
     """Runs openpose on the video, does post-processing, and outputs CSV
        files."""
 
     if body_parts:
         try:
-            body_parts_list = [OpenPoseParts(part) for part in body_parts.split(',')]
+            body_parts_list = [
+                OpenPoseParts(part) for part in body_parts.split(",")
+            ]
         except ValueError as e:
-            click.echo(f'Invalid body-parts value {body_parts}: {e}')
-    elif bodypartsgroup == 'upper':
+            click.echo(f"Invalid body-parts value {body_parts}: {e}")
+    elif bodypartsgroup == "upper":
         body_parts_list = OpenPosePartGroups.UPPER_BODY_PARTS
-    elif bodypartsgroup == 'lower':
+    elif bodypartsgroup == "lower":
         body_parts_list = OpenPosePartGroups.LOWER_BODY_PARTS
 
-    run_openpose(output_dir, openpose_dir, input_video,
-                 input_json, create_model_video, create_overlay_video,
-                 width, height, confidence_threshold, body_parts_list)
+    run_openpose(
+        output_dir,
+        openpose_dir,
+        input_video,
+        input_json,
+        create_model_video,
+        create_overlay_video,
+        width,
+        height,
+        confidence_threshold,
+        body_parts_list,
+    )
 
 
-def run_openpose(output_dir, openpose_dir=None, input_video=None,
-                 input_json=None, create_model_video=False,
-                 create_overlay_video=False, width=None, height=None,
-                 confidence_threshold=0, body_parts=None):
+def run_openpose(
+    output_dir,
+    openpose_dir=None,
+    input_video=None,
+    input_json=None,
+    create_model_video=False,
+    create_overlay_video=False,
+    width=None,
+    height=None,
+    confidence_threshold=0,
+    body_parts=None,
+):
     """Runs openpose on the video, does post-processing, and outputs CSV files.
     Non-click version to work from jupyter notebooks."""
     if not os.path.isdir(output_dir):
-        print(f'No such directory {output_dir}.')
+        print(f"No such directory {output_dir}.")
         exit(1)
 
     # Check input values
@@ -81,19 +143,28 @@ def run_openpose(output_dir, openpose_dir=None, input_video=None,
         exit(1)
 
     if input_json is None and openpose_dir is None:
-        print("You must provide a path to an openpose executable unless you "
-              "provide input json.")
+        print(
+            "You must provide a path to an openpose executable unless you "
+            "provide input json."
+        )
         exit(1)
 
     if input_video is None and create_overlay_video:
-        print("You must provide an input video in order to create an overlay "
-              "video.")
+        print(
+            "You must provide an input video in order to create an overlay "
+            "video."
+        )
         exit(1)
 
-    if input_video is None and create_model_video and \
-       (width == 0 or height == 0):
-        print("You must provide width and height of the original video in "
-              "order to produce a model video.")
+    if (
+        input_video is None
+        and create_model_video
+        and (width == 0 or height == 0)
+    ):
+        print(
+            "You must provide width and height of the original video in "
+            "order to produce a model video."
+        )
         exit(1)
 
     # Get full path to output directory
@@ -103,22 +174,27 @@ def run_openpose(output_dir, openpose_dir=None, input_video=None,
     if input_json:
         path_to_json = os.path.abspath(input_json)
     else:
-        print(f'Detecting poses on {input_video}.')
+        print(f"Detecting poses on {input_video}.")
 
         # Run openpose over the video
         openpose_dir = os.path.abspath(openpose_dir)
 
         # Calling out to the binary seems to be quicker than the python wrapper
         input_video = os.path.realpath(input_video)
-        path_to_json = os.path.join(output_dir, 'json')
-        os.system(f'cd {openpose_dir} && '
-                  './build/examples/openpose/openpose.bin '
-                  f'--video {input_video} '
-                  f'--write_json {path_to_json} --display 0 --render-pose 0')
+        path_to_json = os.path.join(output_dir, "json")
+        os.system(
+            f"cd {openpose_dir} && "
+            "./build/examples/openpose/openpose.bin "
+            f"--video {input_video} "
+            f"--write_json {path_to_json} --display 0 --render-pose 0"
+        )
 
     # Get list of json files
-    json_files = [pos_json for pos_json in os.listdir(path_to_json)
-                  if pos_json.endswith('.json')]
+    json_files = [
+        pos_json
+        for pos_json in os.listdir(path_to_json)
+        if pos_json.endswith(".json")
+    ]
 
     if len(json_files) == 0:
         print(f"No json files found in {path_to_json}. Exiting.")
@@ -138,17 +214,17 @@ def run_openpose(output_dir, openpose_dir=None, input_video=None,
             [0, 1],
             body_parts,
             confidence_threshold,
-            previous_body_keypoints_df
+            previous_body_keypoints_df,
         )
         body_keypoints_df.reset_index()
         body_keypoints_dfs.append(body_keypoints_df)
         previous_body_keypoints_df = body_keypoints_df
 
     if create_model_video:
-        print('Creating model video')
+        print("Creating model video")
 
     if create_overlay_video:
-        print('Creating overlay video')
+        print("Creating overlay video")
 
     if create_model_video or create_overlay_video:
         if not width or not height:
@@ -159,18 +235,18 @@ def run_openpose(output_dir, openpose_dir=None, input_video=None,
 
         visualizer = Visualizer(output_directory=output_dir)
         visualizer.create_videos_from_dataframes(
-            'video',
+            "video",
             body_keypoints_dfs,
             width,
             height,
             create_blank=create_model_video,
             create_overlay=create_overlay_video,
-            video_to_overlay=input_video
+            video_to_overlay=input_video,
         )
 
-    print(f'Saving CSVs to {output_dir}.')
+    print(f"Saving CSVs to {output_dir}.")
     CSVWriter.writeCSV(body_keypoints_dfs, output_dir)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     openpose_cli()
