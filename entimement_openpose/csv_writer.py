525ad779b2683f8212344ab768617a386fd23ffc
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-06-29 20:26:29 +0100

    Reshape the data before outputting to CSV
    This gives 1 row per frame, with x, y, c for each body part in turn in
    the columns.

--- entimement_openpose/csv_writer.py
+++ entimement_openpose/csv_writer.py
@@ -8,6 +8,31 @@ class CSVWriter:
 
         for i in range(n_people):
             person_dfs = [df.iloc[:, i * 3 : i * 3 + 3] for df in dataframes]
+            variable_names = list(person_dfs[0].columns)
+
             all_dfs = pd.concat(person_dfs, keys=range(len(person_dfs)))
 
+            # Unstack to get a row for each frame, with x, y, confidence at top level of cols,
+            # and part at second level of cols
+            all_dfs = all_dfs.unstack()
+            all_dfs.columns.names = ["Variable", "Body Part"]
+
+            # Reorder the column levels
+            all_dfs = all_dfs.reorder_levels(["Body Part", "Variable"], axis=1)
+
+            # Restack to group x, y, confidence for each body part
+            all_dfs = all_dfs.stack(1).unstack()
+
+            # Reindex and rename the variables so they're x, y, c
+            all_dfs = all_dfs.reindex(variable_names, level="Variable", axis=1)
+            all_dfs = all_dfs.rename(
+                columns={
+                    variable_names[0]: "x",
+                    variable_names[1]: "y",
+                    variable_names[2]: "c",
+                },
+                level="Variable",
+            )
+
+            # Output to CSV
             all_dfs.to_csv(os.path.join(output_dir, f"person{i}.csv"))
