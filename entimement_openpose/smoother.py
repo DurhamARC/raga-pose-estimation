42df6cd74bd1e702791d6fa12f098668f3aeecd4
MarionBWeinzierl <marion.weinzierl@durham.ac.uk>
2020-07-16 11:07:04 +0100

    Run black on smoother file

--- entimement_openpose/smoother.py
+++ entimement_openpose/smoother.py
@@ -1,10 +1,11 @@
-#import os
+# import os
 import pandas as pd
 import cv2
 import numpy as np
 from scipy import signal
 
 from .openpose_parts import OpenPoseParts
+
 
 class Smoother:
     """Smoother for data frames created from OpenPose json files
@@ -43,22 +44,30 @@ class Smoother:
         body_keypoints_dfs
         List of smoothed data frames
         """
-        
+
         # Concatenate to one big dataframe all frames (assuming they are sorted wrt to person-order)
         big_body_keypoints_df = pd.concat(body_keypoints_dfs)
-        num_frames = len(body_keypoints_dfs)  
-        num_people = int(len(big_body_keypoints_df.columns)/3)
-        num_bodyparts = len(body_keypoints_dfs[0]) # assuming that we have the same number of bodyparts in each frame
+        num_frames = len(body_keypoints_dfs)
+        num_people = int(len(big_body_keypoints_df.columns) / 3)
+        num_bodyparts = len(
+            body_keypoints_dfs[0]
+        )  # assuming that we have the same number of bodyparts in each frame
 
-        
         # I am sure there is a more phytonic way to do this, but I'll go for the loops now
-        for this_bodypart in range(num_bodyparts): 
-            keypoints_series = big_body_keypoints_df.loc[big_body_keypoints_df.index[this_bodypart]]
-            keypoints_series_smoothed = signal.savgol_filter(keypoints_series, self.smoothing_window, self.polyorder, axis = 0)
-            big_body_keypoints_df.loc[big_body_keypoints_df.index[this_bodypart]] = keypoints_series_smoothed
-               
-        
+        for this_bodypart in range(num_bodyparts):
+            keypoints_series = big_body_keypoints_df.loc[
+                big_body_keypoints_df.index[this_bodypart]
+            ]
+            keypoints_series_smoothed = signal.savgol_filter(
+                keypoints_series, self.smoothing_window, self.polyorder, axis=0
+            )
+            big_body_keypoints_df.loc[
+                big_body_keypoints_df.index[this_bodypart]
+            ] = keypoints_series_smoothed
+
         for i in range(num_frames):
-            body_keypoints_dfs[i] = big_body_keypoints_df.iloc[i*num_bodyparts:(i+1)*num_bodyparts-1] 
-        
+            body_keypoints_dfs[i] = big_body_keypoints_df.iloc[
+                i * num_bodyparts : (i + 1) * num_bodyparts - 1
+            ]
+
         return body_keypoints_dfs
