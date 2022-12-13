85db5b98cd160a9c93c2e97792e313ec3a9cab9e
joannercsheppard <joannesheppard1997@outlook.com>
2022-12-13 16:46:37 +0000

    Adding descriptions to audio_combiner

--- raga_pose_estimation/audio_combiner.py
+++ raga_pose_estimation/audio_combiner.py
@@ -4,33 +4,40 @@ from pymediainfo import MediaInfo
 
 class Audio:
     def __init__(self, input_video_path, output_directory):
-        """Initializes a Visualizer instance with a set of parts to display and
-        an output directory.
-
-        Parameters
-        ----------
-        output_directory : str
-            Path to video output folder. Directory will be created if it
-            doesn't exist.
-
-        Returns
-        -------
-        Visualizer instance
-
-        """
         self.output_directory = output_directory
         self.input_video_path = input_video_path
     
     def _has_audio(self, input_video_path):
+        """Check if video has audio.
+
+        Parameters
+        ----------
+        input_video_path : str
+            String signifying the path to the input video
+
+        Returns
+        -------
+        Booleen value for if audio found
+
+        """
         fileInfo = MediaInfo.parse(input_video_path)
         return any([track.track_type == 'Audio' for track in fileInfo.tracks])
 
     def _extract_audio(self, input_video_path, output_directory):
-        """
+        """Extracts audio from an input video
+
         Parameters
-        ---------
-        Return:
+        ----------
+        input_video_path : Str
+            String signifying the path to the input video
+
+        output_directory: Str
+            String signifying output directory path
+
+        Returns
+        -------
         None
+
         """
         command = "ffmpeg -i "
         command += input_video_path
@@ -42,6 +49,21 @@ class Audio:
         return None
 
     def _attach_audio(self, output_directory):
+        """Attaches audio to the overlay video in the output directory
+
+        Parameters
+        ----------
+        input_video_path : Str
+            String signifying the path to the input video
+
+        output_directory: Str
+            String signifying output directory path
+
+        Returns
+        -------
+        None
+
+        """
         command = "ffmpeg -i "
         command += output_directory
         command += "/video_overlay.mp4"
@@ -55,6 +77,21 @@ class Audio:
         return None
 
     def audio_combiner(self, input_video_path, output_directory):
+        """Combines audio to output overlay video
+
+        Parameters
+        ----------
+        input_video_path : Str
+            String signifying the path to the input video
+
+        output_directory: Str
+            String signifying output directory path
+
+        Returns
+        -------
+        None
+
+        """
         if self._has_audio(input_video_path):
             self._extract_audio(input_video_path, output_directory)
             self._attach_audio(output_directory)
